78dd0078206dbab8af05db77faa65d291ed3020d
Gabri3lCarneiro <gabrielcarneirodesouza2.0@gmail.com>
2024-09-18 02:35:10 -0300

    asdg

--- Livraria-spring-web-main/src/main/java/GRUPO/_1/FLC21444DS/Livraria/entidades/Reservas.java
+++ Livraria-spring-web-main/src/main/java/GRUPO/_1/FLC21444DS/Livraria/entidades/Reservas.java
@@ -6,13 +6,10 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
